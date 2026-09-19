ced7fb2af8dacefdb3824812f70a1aa92fd6cd65
Stephanie Otteson <vibrantimpact@myyahoo.com>
2026-09-18 22:02:56 -0600

    Initial Commit

---
+++ AmbiFlow/AmbiFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmbiFlowApp.swift
+//  AmbiFlow
+//
+//  Created by stephanie otteson on 2026-09-18.
+//
+
+import SwiftUI
+
+@main
+struct AmbiFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
